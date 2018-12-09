233db169fde954f60dd65969954200124784d0a5
lukanemso <45319341+lukanemso@users.noreply.github.com>
2018-12-09 15:10:33 +0400

    l601

---
+++ L601/script.js
@@ -0,0 +1,8 @@
+let studentId = prompt("Enter Student id"),
+  selStudent = document.querySelector(`div[data-student-id='${studentId}']`);
+
+if (selStudent) {
+  selStudent.style.backgroundColor = "green";
+}
+
+//
